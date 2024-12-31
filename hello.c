6fa32a9815d2e40f70d5fb3da7a171ace27a3b1e
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-12-31 11:57:30 +0530

    bro

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" I like pizza \n");
+    printf(" 1\t2\t3 \n 4");
+    printf(" \"I like ice cream\" \n");
+    return 0;
+}
---
+++ nino.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 21;
+    char name[] = "BRO";
+    float gpa = 4.9;
+    char grade = 'A';
+
+    printf("My age is %d\n", age);
+    printf("My name is %s\n", name);
+    printf("My gpa is %f not so great \n", gpa);
+    printf("My grade is %c , yeah quite good", grade);
+}
