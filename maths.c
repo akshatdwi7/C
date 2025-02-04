c32ff4df275c093f2a1aa9505bfb2ce76024a7fe
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-05 00:02:55 +0530

    m

--- maths.c
+++ maths.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
 {
     int x = 90;
     int y = 78;
     float z = (float)x / (float)y;
-    printf("%f", z);
+    printf("%f\n", z);
+    double o = sqrt(38);
+    printf("%lf", o);
 }
---
+++ userinput /anotheruserinput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int age;
+    char name[25] = ""; // bytes
+    printf("what do you think is your name is?\n");
+    // scanf("%s", &name);
+    fgets(name, 25, stdin); // using this function we can read any white spaces also we don't need to add & before the name
+    name[strlen(name) - 1] = '\0';
+    printf("what do you think is your age is?\n");
+    scanf("%d", &age);
+    printf("My name is lol as of now is  %s\n %d", name, age);
+}
