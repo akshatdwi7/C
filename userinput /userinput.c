f2dd905a328ba38180ac1ec0406069f1c6406d13
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-04 23:25:48 +0530

    Update userinput.c

--- userinput /userinput.c
+++ userinput /userinput.c
@@ -8,5 +8,6 @@ int main()
     scanf("%d", &age);
     // fgets () is used to read a line of text from the standard input.
     printf("your age is %d years old\n", age);
+    printf(" My name is");
     return 0;
 }
