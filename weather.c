e3ce4ff149209ca2a663c77cecafd8b0b8db6d8d
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-06 02:43:07 +0530

    f
    f

---
+++ weather.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int n = 25;
+    int w = false;
+    if (n <= 30 || w)
+    {
+        printf("this is th egood weather");
+    }
+    else
+    {
+        printf(" this is the bad weather");
+    }
+}
