e240cb8e8f059d5930b73dc37360a6fc4d602c90
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-11 23:21:26 +0530

    learing dsa

---
+++ DSA/selectionsort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void sort(){
+    
+}
+
+
+int main(){
+
+ int array[]={26, 89, 55, 12, 1, 7, 2, 44};
+ int size=sizeof(array)/sizeof(array[0]);
+
+ sort(array,size);
+ printsort(array, size);
+
+
+}
--- arrays/2darray.c
+++ arrays/2darray.c
@@ -1,28 +1,33 @@
-/*
-2D array = an array where each element is an entire
- araay useful if you need matrix, grid or table data
-
-*/
 #include <stdio.h>
+
+#include <string.h>
 
 int main()
 {
 
-    int a[2][3] = {
-        {1, 2, 3},
-        {4, 5, 6}};
-    a[0][2] = 0;
-    int rows;
-    int columns;
-    rows = sizeof(a) / sizeof(a[0]);          // number of rows
-    columns = sizeof(a[0]) / sizeof(a[0][0]); // sizeof(a[0]) is the size of the first row
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
+    char cars[2][5][10] = {
         {
-            printf("%d", a[i][j]);
+            "tesla", "ford", "mg", "bmw", "toyota"
+        },
+        {
+            "swift ", "alto", "i20", "bmw" , "mercedes"
+        },
+    };
+
+    int rows = sizeof(cars) / sizeof(cars[0]);
+    int columns = sizeof(cars[0]) / sizeof(cars[0][0]);
+   
+
+    for (int i = 0; i < rows; i++) {
+
+        for (int j = 0; j < columns; j++) {
+
+            printf(" %s", cars[i][j]);
         }
-        printf("  \n");
+
+        printf("\n");
     }
+    return 0;
+
+
 }
