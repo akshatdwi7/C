8e7ae4337c416a6d0252409a7a37d4add3f60110
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-03-20 22:02:52 +0530

    n

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@ int main()
 {
     printf(" I like pizza \n");
     printf(" 1\t2\t3 \n 4");
-    printf(" \"I like ice cream\" \n"); // if we want to print a string with
-    // double quotes, we need to use \" to escape the double quotes
-    return 0;
+    printf(" \"I like ice cream\" \n");
+    printf("hey the wo4ld isnew ") // if we want to print a string with
+        // double quotes, we need to use \" to escape the double quotes
+        return 0;
 }
